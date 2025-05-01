ce69e3aebf7cf41770033c4db0a6be89b8ef43c2
mpotpinka <mpotpinka@classlink.com>
2025-04-30 22:28:40 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,20 @@
+console.log("main.js loaded");
+document.querySelector("button").addEventListener("click", apiRequest);
+
+async function apiRequest() {
+  const username = document.querySelector("input").value;
+  try {
+    const response = await fetch(`/api/${username}`);
+    const data = await response.json();
+
+    console.log(data);
+    
+	let skillLevels = ""
+	data.skills.forEach(skill => {
+		skillLevels += `${skill.name}: Level ${skill.level}\n`
+	})
+    document.querySelector("#info").innerText = skillLevels;
+  } catch (error) {
+    console.log(error);
+  }
+}
---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const fetch = require("node-fetch");
+const path = require("path");
+const cors = require("cors");
+
+app.use(cors());
+
+app.get("/", (request, response) => {
+  response.sendFile(path.join(__dirname + "/index.html"));
+});
+
+app.get("/main.js", (req, res) => {
+  res.sendFile(path.join(__dirname, "main.js"));
+});
+
+app.get("/api/:username", async (request, response) => {
+  const { username } = request.params;
+  try {
+    const apiResponse = await fetch(
+      `https://secure.runescape.com/m=hiscore_oldschool/index_lite.json?player=${username}`
+    );
+    const data = await apiResponse.text(); // Get the response as text
+    response.send(data); // Send it to the frontend
+  } catch (err) {
+    response.status(500).send("Error fetching OSRS data");
+  }
+});
+
+const PORT = 8000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
